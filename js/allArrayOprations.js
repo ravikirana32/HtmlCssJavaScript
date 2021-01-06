68c17ad5ee850a6abd936f9ec598818f576439a9
ravikirana32 <ravikirana32@gmail.com>
2021-01-06 17:39:39 +0530

    Array copywithin() EXample

--- js/allArrayOprations.js
+++ js/allArrayOprations.js
@@ -4,8 +4,17 @@ let array2=[100,200,300,400,500]
 console.log(array1.concat(array2));//OUTPUT:[1,2,3,4,5,6,7,8,9,10,100,200,300,400,500]
 console.log(array1.concat(array2,array1));//OUTPUT:[1,2,3,4,5,6,7,8,9,10,100,200,300,400,500,1,2,3,4,5,6,7,8,9,10]
 
-//Array.constructor(Initialization array)
+//Array.constructor(Initialization array) it is same for Object and strings also
 let ar=["R","A","V","I"];
 
 console.log(ar.constructor(array2));//[100,200,300,400,500]
 console.log(ar);
+
+//The copyWithin(coping value position,copy to position) method copies array elements to another position in the array, overwriting the existing values.
+//This method will never add more items to the array.
+//Note: this method overwrites the original array.
+
+let fruits=["apple","Mango","finapple","Orrange","Banana"];
+fruits.copyWithin(1,4)
+console.log(fruits);//["apple", "Banana", "finapple", "Orrange", "Banana"]
+
