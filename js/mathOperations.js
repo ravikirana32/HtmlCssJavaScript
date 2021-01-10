127e1430db119eebaace4cc051d3177bedab3b92
Ravi Kirana <ravikirana32@gmail.com>
2021-01-10 12:35:54 +0530

    Math operations

---
+++ js/mathOperations.js
@@ -0,0 +1,111 @@
+//The abs() method returns the absolute value of a number.
+//Math.abs(x)
+console.log("\n---------------------  abs()  --------------");
+console.log(Math.abs(-7));//7
+console.log(Math.abs(-15.35));//15.35
+
+//The cbrt() method returns the cubic root of a number.
+console.log("\n---------------------  cbrt()  --------------");
+console.log(Math.cbrt(8));//2
+console.log(Math.cbrt(27));//3
+console.log(Math.cbrt(64));//4
+console.log(Math.cbrt(4));//1.5874010519681996
+
+// The ceil() method rounds a number UPWARDS to the nearest integer, and returns the result.
+// If the passed argument is an integer, the value will not be rounded.
+console.log("\n---------------------  ceil()  --------------");
+console.log(Math.ceil(1));//1
+console.log(Math.ceil(1.1));//2
+console.log(Math.ceil(1.2));//2
+console.log(Math.ceil(1.3));//2
+console.log(Math.ceil(1.4));//2
+console.log(Math.ceil(1.5));//2
+console.log(Math.ceil(1.6));//2
+console.log(Math.ceil(1.7));//2
+console.log(Math.ceil(1.8));//2
+console.log(Math.ceil(1.9));//2
+
+
+//The exp() method returns the value of E^x, where E is Euler's number (approximately 2.7183) and x is the number passed to it.
+//Math.exp(x)
+console.log("\n---------------------  exp()  --------------");
+console.log(Math.exp(3));//20.085536923187668
+console.log(Math.exp(2));//7.38905609893065
+
+
+// The floor() method rounds a number DOWNWARDS to the nearest integer, and returns the result.
+// If the passed argument is an integer, the value will not be rounded.
+console.log("\n---------------------  floor()  --------------");
+console.log(Math.floor(1));//1
+console.log(Math.floor(1.1));//1
+console.log(Math.floor(1.2));//1
+console.log(Math.floor(1.3));//1
+console.log(Math.floor(1.4));//1
+console.log(Math.floor(1.5));//1
+console.log(Math.floor(1.6));//1
+console.log(Math.floor(1.7));//1
+console.log(Math.floor(1.8));//1
+console.log(Math.floor(1.9));//1
+
+
+//The max() method returns the number with the highest value.
+//Match.max(2,3,4,10,5,20.....)
+console.log("\n---------------------  max()  --------------");
+console.log(Math.max(2,3,4,10,5,20));//20
+
+
+//The min() method returns the number with the lowest value.
+//Match.min(2,3,4,10,5,20.....)
+console.log("\n---------------------  min()  --------------");
+console.log(Math.min(2,3,4,10,5,20));//2
+
+//The PI property returns the ratio of a circle's area to the square of its radius, approximately 3.14
+console.log("\n---------------------  PI  --------------");
+console.log(Math.PI);//3.14
+
+
+//The pow() method returns the value of x to the power of y (xy).
+//Math.pow(x, y)
+//Return the value of the number 4 to the power of 3 (4*4*4): Math.pow(4, 3)
+console.log("\n---------------------  pow()  --------------");
+console.log(Math.pow(4,3));//64
+console.log(Math.pow(2,5));//32
+
+
+//The random() method returns a random number from 0 (inclusive) up to but not including 1 (exclusive).
+//Math.random()
+console.log("\n---------------------  random()  --------------");
+console.log(Math.random());//0.46
+
+
+// The round() method rounds a number to the nearest integer.
+// Note: 2.49 will be rounded down (2), and 2.5 will be rounded up (3).
+console.log("\n---------------------  round()  --------------");
+console.log(Math.round(1));//1
+console.log(Math.round(1.1));//1
+console.log(Math.round(1.2));//1
+console.log(Math.round(1.3));//1
+console.log(Math.round(1.4));//1
+console.log(Math.round(1.5));//2
+console.log(Math.round(1.6));//2
+console.log(Math.round(1.7));//2
+console.log(Math.round(1.8));//2
+console.log(Math.round(1.9));//2
+
+
+// The sign() method checks whether a number is negative, positive or zero.
+// If the number is positive, this method returns 1.
+// If the number is negative, it returns -1.
+// If the number is zero, it returns 0.
+console.log("\n---------------------  sign()  --------------");
+console.log(Math.sign(-20));//-1
+console.log(Math.sign(20));//1
+console.log(Math.sign(0));//0
+
+
+//The sqrt() method returns the square root of a number.
+//Math.sqrt(value)
+console.log("\n---------------------  sqrt()  --------------");
+console.log(Math.sqrt(4));//2
+console.log(Math.sqrt(64));//8
+console.log(Math.sqrt(100));//10
